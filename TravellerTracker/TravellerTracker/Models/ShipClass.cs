693dee513bcd177780960300e1e9646f3dd8bb54
Craig Oliver <coliver988@gmail.com>
2017-09-08 21:25:50 -0400

    reverted dropping columns (not yet supported!); new migrations

--- TravellerTracker/TravellerTracker/Models/ShipClass.cs
+++ TravellerTracker/TravellerTracker/Models/ShipClass.cs
@@ -16,7 +16,6 @@ namespace Traveller.Models
         public int Power { get; set; }
         public int Cargo { get; set; }
 
-        public int Fuel { get; set; }
         public string Description
         {
             get { return string.Format("{0} {1} tons, {2}/{3}/{4} jump/man/power", Name, dTons, Jump, Man, Power); }
