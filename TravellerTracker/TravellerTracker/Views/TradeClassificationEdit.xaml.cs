11c00c1153facfd58151a0cc2e2f773b6c59007d
Craig Oliver <coliver988@gmail.com>
2017-11-03 20:37:59 -0400

    save & add new trade codes. needs work on the add

--- TravellerTracker/TravellerTracker/Views/TradeClassificationEdit.xaml.cs
+++ TravellerTracker/TravellerTracker/Views/TradeClassificationEdit.xaml.cs
@@ -1,18 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
+﻿using System.Linq;
 using Traveller.Models;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -28,6 +17,19 @@ namespace TravellerTracker.Views
         {
             this.InitializeComponent();
             tc = TravellerTracker.App.DB.TradeClassifications.Where(x => x.TradeClassificationID == id).First();
+        }
+
+        private void btnSave(object sender, RoutedEventArgs e)
+        {
+            App.DB.SaveChangesAsync();
+        }
+
+        private void btnNew(object sender, RoutedEventArgs e)
+        {
+            tc = new TradeClassification();
+            tc.Name = "new trade classification";
+            App.DB.TradeClassifications.Add(tc);
+            this.DataContext = tc;
         }
     }
 }
