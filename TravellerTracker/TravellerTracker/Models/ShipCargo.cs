a6049272b0016304ee6e02b3a4da2f14af7db485
Craig Oliver <coliver988@gmail.com>
2017-11-06 11:20:40 -0500

    added tonnage, origin world ID and ship specific code

--- TravellerTracker/TravellerTracker/Models/ShipCargo.cs
+++ TravellerTracker/TravellerTracker/Models/ShipCargo.cs
@@ -11,5 +11,13 @@ namespace Traveller.Models
         public int ShipCargoID { get; set; }
         public int ShipID { get; set; }
         public int CargoID { get; set; }
+
+        // how many actual tons are carried
+       
+        public int dTons { get; set;}
+
+        public string CargoCode { get; set; }  // BITS or other cargo code specific to this
+
+        public int OriginWorldID { get; set; }    // where it originates from
     }
 }
