83b5c005068d123426970417c87e2a39c3cafc52
Craig Oliver <coliver988@gmail.com>
2017-12-08 12:57:52 -0500

    cargotype migration starting BITS cargo strings

--- TravellerTracker/TravellerTracker/Models/ShipCargo.cs
+++ TravellerTracker/TravellerTracker/Models/ShipCargo.cs
@@ -1,16 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Traveller.Models
+﻿namespace Traveller.Models
 {
     public class ShipCargo
     {
+        public enum CargoTypes
+        {
+            Speculative,
+            Major,
+            Minor,
+            Incidental,
+            Mail
+        }
+
         public int ShipCargoID { get; set; }
         public int ShipID { get; set; }
         public int CargoID { get; set; }
+        public CargoTypes CargoType { get; set; }
 
         public int dTons { get; set;}           // how many actual tons are carried      
         public string CargoCode { get; set; }  // BITS or other cargo code specific to this
---
+++ TravellerTracker/TravellerTracker/Support/BITSCargoCode.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravellerTracker.Support
+{
+    public class BITSCargoCode
+    {
+        Dictionary<char, string> ContainerType = new Dictionary<char, string>()
+        {
+            {'0', "Open Frame" },
+            {'1', "Enclosed, unsecured" },
+            {'2', "Enclosed, secured" },
+            {'3', "Hermetically sealed, secure" },
+            {'4', "Hermetically sealed, pressurized" }
+        };
+        Dictionary<char, string> ContainerSize = new Dictionary<char, string>()
+        {
+            {'0', "C0 (small container up to 0.5x0.5x05 m" },
+            {'1', "C1 (1 cubic meter 1x1x1 meter" },
+            {'2', "C3 (3 cubic meter, 3x1x1 meter" },
+            {'3', "C9 (9 cubic meter, 3x3x1 meter" },
+            {'4', "C27 (27 cubic meter, 3x3x3 meter" },
+            {'5', "C54 (54 cubic meter, 3x3x6 meter" },
+            {'6', "C81 (81 cubic meter, 3x3x9 meter" },
+            {'7', "C108 (108 cubic meter, 3x6x6 meter" },
+            {'8', "C162 (162 cubic meter, 3x6x9 meter" },
+            {'9', "non-standard configuration" }
+        };
+        Dictionary<char, string> ContainerMass = new Dictionary<char, string>()
+        {
+            {'0', "Under 1 kg" },
+            {'1', "1 - 10 kg" },
+            {'2', "10 - 100 kg" },
+            {'3', "100 kg - 1 metric ton" },
+            {'4', "1 - 10 tonnes" },
+            {'5', "10 - 100 tonnes" },
+            {'6', "100 - 200 tonnes" },
+            {'7', "200 - 500 tonnes" },
+            {'8', "500 - 1000 tonnes" },
+            {'9', "1000+ tonnes" }
+        };
+        Dictionary<char, string> AtmosphericRange = new Dictionary<char, string>()
+        {
+            {'0', "Vaccum" },
+            {'1', "Trace (0.0 - 0.1 atm)" },
+            {'2', "Very thin (0.1 - 0.4 atm" },
+            {'3', "Thin (0.4 - 0.75 atm" },
+            {'4', "Standard (0.75 - 1.4 atm" },
+            {'5', "Dense (1.4 - 2.5 atm" },
+            {'6', "High Pressure ( (2.5 - 5.0 atm" },
+            {'7', "Very High Pressure (5.0 - 25 atm" },
+            {'8', "Extreme (25+ atm" }
+        };
+        Dictionary<char, string> TemperatureRange = new Dictionary<char, string>()
+        {
+            {'X', "Below 50C" },
+            {'0', "-50C" },
+            {'1', "-40C" },
+            {'2', "-30C" },
+            {'3', "-20C" },
+            {'4', "-10C" },
+            {'5', "0C" },
+            {'6', "10C" },
+            {'7', "20C" },
+            {'8', "30C" },
+            {'9', "40C" },
+            {'A', "50C" },
+            {'B', "60C" },
+            {'C', "70C" },
+            {'D', "80C" },
+            {'E', "90C" },
+            {'F', "100C" },
+            {'Y', "100+C" }
+        };
+        Dictionary<char, string> HumidityRange = new Dictionary<char, string>()
+        {
+            {'0', "0%" },
+            {'1', "1 - 10%" },
+            {'2', "11 - 20%" },
+            {'3', "21 - 30%" },
+            {'4', "31 - 40%" },
+            {'5', "41 - 50%" },
+            {'6', "51 - 60%" },
+            {'7', "61 - 70%" },
+            {'8', "71 - 80%" },
+            {'9', "81 - 90%" },
+            {'A', "Any" },
+        };
+        Dictionary<char, string> MaxGravityRange = new Dictionary<char, string>()
+        {
+            {'0', "0G" },
+            {'1', "1G" },
+            {'2', "2G" },
+            {'3', "3G" },
+            {'4', "4G" },
+            {'5', "5G" },
+            {'6', "6G" },
+            {'7', "7G" },
+            {'8', "8G" },
+            {'9', "9G" },
+            {'A', "9G+" },
+            {'X', "Do not store in 0G" }
+        };
+        Dictionary<char, string> CargoType = new Dictionary<char, string>()
+        {
+            {'0', "Solid" },
+            {'1', "Powder" },
+            {'2', "Solid/liquid mixture" },
+            {'3', "Solid/gas mixture" },
+            {'4', "Liquid" },
+            {'5', "Gas/Liquid mixture" },
+            {'6', "Solidified gas" },
+            {'7', "Liquified gas" },
+            {'8', "Compressed gas" },
+            {'9', "Rarified gas" },
+            {'A', "Gas plasma" },
+            {'B', "Assorted forms (mixed)" },
+            {'C', "Live flora" },
+            {'D', "Live fauna" },
+            {'E', "Unusual" },
+        };
+    }
+}
