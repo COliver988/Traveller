c61f4815c9dc579f01e1fb0a32bd4dcdfc9a3fea
Craig Oliver <coliver988@gmail.com>
2017-12-09 15:54:07 -0500

    got the user control for bulk cargo working

--- TravellerTracker/TravellerTracker/UserControls/BulkCargoSelector.xaml.cs
+++ TravellerTracker/TravellerTracker/UserControls/BulkCargoSelector.xaml.cs
@@ -1,19 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
+﻿using System.Collections.Generic;
 using Traveller.Models;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-using System.ComponentModel;
 
 // The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
 
@@ -22,14 +10,13 @@ namespace TravellerTracker.UserControls
     public sealed partial class BulkCargoSelector : UserControl
     {
         public static readonly DependencyProperty CargoListProperty = DependencyProperty.Register("CargoList", typeof(List<BulkCargo>), typeof(BulkCargoSelector), new PropertyMetadata(null));
-        public List<BulkCargo> CargoList { get; set; }
+        public IEnumerable<BulkCargo> CargoList { get; set; }
         public static readonly DependencyProperty WorldNameProperty = DependencyProperty.Register("WorldName", typeof(string), typeof(BulkCargoSelector), new PropertyMetadata(null));
         public string WorldName { get; set; }
 
         public BulkCargoSelector()
         {
             this.InitializeComponent();
-            lstCargo.ItemsSource = CargoList;
         }
     }
 }
