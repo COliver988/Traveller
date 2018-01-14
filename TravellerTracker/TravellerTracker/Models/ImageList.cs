9804a3b976cff3487b9c2afb5f451a1cd99220bb
Craig Oliver <coliver988@gmail.com>
2018-01-14 10:05:58 -0500

    added new model: ImageList added ListBox style to dictionary

---
+++ TravellerTracker/TravellerTracker/Models/ImageList.cs
@@ -0,0 +1,10 @@
+﻿namespace TravellerTracker.Models
+{
+    public class ImageList
+    {
+        public int ImageListID { get; set; }
+        public int ShipID { get; set; }
+        public int WorldID { get; set; }
+        public byte[] theImage { get; set; }
+    }
+}
