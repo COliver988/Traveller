a549c7b6b43ed3a04be4d4ef39d9080206876d7f
Craig Oliver <coliver988@gmail.com>
2018-01-28 17:47:52 -0500

    image carousel working on ship view

--- TravellerTracker/TravellerTracker/UserControls/ImageViewer.xaml.cs
+++ TravellerTracker/TravellerTracker/UserControls/ImageViewer.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
+using TravellerTracker.Models;
+using TravellerTracker.Support;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Imaging;
 
 // The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
 
@@ -34,15 +38,26 @@ namespace TravellerTracker.UserControls
             Loaded += ImageViewer_Loaded;
         }
 
-        private void ImageViewer_Loaded(object sender, RoutedEventArgs e)
+        private async void ImageViewer_Loaded(object sender, RoutedEventArgs e)
         {
-            if (ShipID > 0)
-                ctlCarousel.ItemsSource = App.DB.ImageLists.Where(x => x.ShipID == ShipID);
-            else
-                if (WorldID > 0)
-                ctlCarousel.ItemsSource = App.DB.ImageLists.Where(x => x.WorldID == WorldID);
+            List<MyImageList> myImageList = new List<MyImageList>();
+            List<ImageList> imageList = new List<ImageList>();
+            if (ShipID > 0) imageList = App.DB.ImageLists.Where(x => x.ShipID == ShipID).ToList();
+            else if (WorldID > 0) imageList = App.DB.ImageLists.Where(x => x.WorldID == WorldID).ToList();
 
-            var y = App.DB.ImageLists.Where(x => x.ShipID == ShipID);
+            ImageHandler ih = new ImageHandler();
+            foreach (ImageList item in imageList)
+            {
+                BitmapImage i = await ih.bytesToImage(item.theImage);
+                myImageList.Add(new MyImageList() { bmp = i, Description = item.Description });
+            }
+            ctlCarousel.ItemsSource = myImageList;
+        }
+
+        private class MyImageList
+        {
+            public BitmapImage bmp { get; set; }
+            public string Description { get; set; }
         }
     }
 }
