b7337edf4370e25d1cf8e9bc9e5240c5808d150c
Craig Oliver <coliver988@gmail.com>
2019-09-02 14:47:10 -0400

    ship edit basic functionality is there

--- TravellerTrackerv5/ShipTracker/ShipTracker/ShipEditor.xaml.cs
+++ TravellerTrackerv5/ShipTracker/ShipTracker/ShipEditor.xaml.cs
@@ -35,14 +35,19 @@ namespace ShipTracker
         {
             List<Models.ShipClass> classes = App.DB.ShipClass.ToList();
             classList.ItemsSource = classes;
+            if (theShip.ShipClassID > 0)
+                classList.SelectedItem = classes.Where(x => x.id == theShip.ShipClassID).FirstOrDefault();
         }
 
         private void ClassList_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
         {
             ComboBox cb = sender as ComboBox;
             Models.ShipClass shipClass = (Models.ShipClass)cb.SelectedItem;
-            theShip.ShipClassID = shipClass.id;
-            App.DB.SaveChangesAsync();
+            if (shipClass != null)
+            {
+                theShip.ShipClassID = shipClass.id;
+                App.DB.SaveChangesAsync();
+            }
         }
 
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
