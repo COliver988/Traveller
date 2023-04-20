bb4a7e2a293126c3ad48f22d06947f6355224196
Craig Oliver <coliver988@gmail.com>
2023-04-20 16:47:02 -0400

    updated to .Net 7; restarting project

---
+++ TrackerCore/TrackerCore/Migrations/20230420204608_AddWorld.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TrackerCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddWorld : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "WorldID",
+                table: "Ships",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "currentWorld",
+                table: "Ships",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Worlds",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Worlds", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Worlds");
+
+            migrationBuilder.DropColumn(
+                name: "WorldID",
+                table: "Ships");
+
+            migrationBuilder.DropColumn(
+                name: "currentWorld",
+                table: "Ships");
+        }
+    }
+}
--- TravellerTrackerv5/ShipTracker/ShipTracker/Models/ShipClass.cs
+++ TravellerTrackerv5/ShipTracker/ShipTracker/Models/ShipClass.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ShipTracker.Models
+﻿namespace ShipTracker.Models
 {
     public class ShipClass
     {
